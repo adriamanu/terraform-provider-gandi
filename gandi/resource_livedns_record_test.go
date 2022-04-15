e71eaa5635c78a35199c0fc4f7dd50fac8f5e697
Emmanuel Adrian <emmanuel.adrian@linkfluence.com>
2022-04-15 18:04:14 +0200

    unit tests

--- gandi/resource_livedns_record_test.go
+++ gandi/resource_livedns_record_test.go
@@ -2,6 +2,7 @@ package gandi
 
 import (
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/logging"
@@ -72,5 +73,87 @@ func TestAccRecord_manually_removed(t *testing.T) {
 				Config:    testAccConfigRecord(),
 			},
 		},
+	})
+}
+
+func TestKeepUniqueRecords(t *testing.T) {
+	recordsListFromApi := []string{"record_one", "record_two", "record_three"}
+	recordsListFromTerraform := []string{"record_one", "tf_record_two"}
+
+	t.Run("Remove duplicated record from records list", func(t *testing.T) {
+		recordsList := append(recordsListFromApi, recordsListFromTerraform...)
+		shortenedList := keepUniqueRecords(recordsList)
+
+		if !(len(shortenedList) == 4) {
+			t.Errorf("Amount of records should have been decreased by one.")
+		}
+	})
+}
+
+func TestIfRecordIfWrappedWithQuote(t *testing.T) {
+	t.Run("wrapped with quotes", func(t *testing.T) {
+		wrappedRecord := "\"192.168.0.1\""
+		if !isRecordWrappedWithQuotes(wrappedRecord) {
+			t.Errorf("%s records is wrapped with quotes.", wrappedRecord)
+		}
+	})
+
+	t.Run("suffix quote", func(t *testing.T) {
+		wrappedRecord := "192.168.0.1\""
+		if isRecordWrappedWithQuotes(wrappedRecord) {
+			t.Errorf("%s records is not wrapped with quotes.", wrappedRecord)
+		}
+	})
+
+	t.Run("prefix quote", func(t *testing.T) {
+		wrappedRecord := "\"192.168.0.1"
+		if isRecordWrappedWithQuotes(wrappedRecord) {
+			t.Errorf("%s records is not wrapped with quotes.", wrappedRecord)
+		}
+	})
+
+	t.Run("no quotes", func(t *testing.T) {
+		wrappedRecord := "192.168.0.1"
+		if isRecordWrappedWithQuotes(wrappedRecord) {
+			t.Errorf("%s records is not wrapped with quotes.", wrappedRecord)
+		}
+	})
+}
+
+func TestIfRecordsListContainsRecord(t *testing.T) {
+	recordToCheck := "10.10.0.0"
+	t.Run("contains record at first index", func(t *testing.T) {
+		recordsList := []string{"192.168.1.1", "10.10.0.0", "0.0.0.0"}
+		index, exists := containsRecord(recordsList, recordToCheck)
+		if !exists && index != 1 {
+			t.Errorf("%s should be existing and being at index 1 of the records list", recordToCheck)
+		}
+	})
+
+	t.Run("records list does not contain the desired record", func(t *testing.T) {
+		recordsList := []string{"192.168.1.1", "10.10.10.10", "0.0.0.0"}
+		_, exists := containsRecord(recordsList, recordToCheck)
+		if exists {
+			t.Errorf("records list should not contain the desired record")
+		}
+	})
+}
+
+func TestRemoveRecordFromValuesList(t *testing.T) {
+	recordsList := []string{"192.168.1.1", "10.10.10.10", "0.0.0.0"}
+	t.Run("remove record at first index", func(t *testing.T) {
+		shortenedList := removeRecordFromValuesList(recordsList, 0)
+		awaitedList := []string{"10.10.10.10", "0.0.0.0"}
+		if !reflect.DeepEqual(shortenedList, awaitedList) {
+			t.Errorf("shortenedList should only contains 2 elements")
+		}
+	})
+
+	t.Run("remove record at third index", func(t *testing.T) {
+		shortenedList := removeRecordFromValuesList(recordsList, 2)
+		awaitedList := []string{"192.168.1.1", "10.10.10.10"}
+		if !reflect.DeepEqual(shortenedList, awaitedList) {
+			t.Errorf("shortenedList should only contains 2 elements")
+		}
 	})
 }
